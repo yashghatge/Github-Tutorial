98ad6c4d6432a3e3ce712795de93381a8bd660a6
Yash Ghatge <Ghatge.yash@yahoo.com>
2022-01-18 19:43:20 -0500

    First Commit

---
+++ quiz.js
@@ -0,0 +1,25 @@
+﻿//////Importing core module fs
+////const fs = require('fs');
+
+////__dirname = "C:\Users\ghatg\source\repos\Lab_InClass";
+////fs.readdir(__dirname, (err, files) => {
+////    if (err)
+////        console.log(err);
+////    else {
+////        console.log("\nCurrent directory filenames:");
+////        files.forEach(file => {
+////            console.log(file);
+////        })
+////    }
+////})
+
+
+
+const { v4: uuidv4 } = require('uuid');
+const str = uuidv4();
+console.log(str);
+
+
+
+
+
